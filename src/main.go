109ec20776f33204a7b1718f84dfa5778b8de997
Andrii Vodolagin <andrii.vodolahin@rhinocodes.com>
2022-05-19 23:34:05 +0200

    - load data

--- src/main.go
+++ src/main.go
@@ -2,22 +2,58 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/AlekSi/pointer"
-	gamp "github.com/olebedev/go-gamp"
+	gampClient "github.com/olebedev/go-gamp"
 	"github.com/olebedev/go-gamp/client/gampops"
-	"log"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
 
+func sendData(gampClient *gampops.Client) error {
+	fmt.Println("...initialized")
+
+	ch := make(chan error)
+
+	url := "https://api.apilayer.com/exchangerates_data/latest?symbols=usd,chf,jpy,gbp&base=eur"
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("apikey", os.Getenv("APILAYER_API_KEY"))
+
+	go func() {
+		for {
+			res, err := client.Do(req)
+			if res.Body != nil {
+				defer res.Body.Close()
+			}
+			body, err := ioutil.ReadAll(res.Body)
+
+			err = gampClient.Collect(
+				gampops.NewCollectParams().
+					WithCid(pointer.ToString("266035594")).
+					WithT("event").
+					WithEc(pointer.ToString(string(body))).
+					WithEa(pointer.ToString("data_sent")).
+					WithUa(pointer.ToString("local pc")).
+					WithDp(pointer.ToString("/test")),
+			)
+			if err != nil {
+				ch <- err
+				break
+			}
+
+			fmt.Printf("%v\n ...sleeping an hour", string(body))
+			time.Sleep(10 * time.Minute)
+		}
+	}()
+
+	return <-ch
+}
+
 func main() {
-	client := gamp.New(context.Background(), "UA-XXXXXXXX-X")
-	err := client.Collect(
-		gampops.NewCollectParams().
-			WithCid(pointer.ToString("42")).
-			WithT("event").
-			WithEc(pointer.ToString("Category")).
-			WithEa(pointer.ToString("Action")),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	err := sendData(gampClient.New(context.Background(), "UA-104390508-1"))
+
+	fmt.Println(err)
 }
